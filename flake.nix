4f2ac9702169993dfb7150c6ee3a3bd876e3f9f7
Flokkq <webcla21@htl-kaindorf.at>
2024-11-02 20:37:29 +0100

    init(rust): init with rust

--- flake.nix
+++ flake.nix
@@ -1,25 +1,44 @@
 {
-  description = "A basic flake for git-cliff";
+  description = "Basic Rust flake for smd-store";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem(system:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
       in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-                pkgs.git-cliff
-                pkgs.gnupg
-            ];
-          };
-        }
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          buildInputs = [
+            (rust-bin.selectLatestNightlyWith( toolchain: toolchain.default.override {
+              extensions= [ "rust-src" "rust-analyzer" ];
+              targets = [];
+            }))
+
+            pkg-config
+            openssl
+            sqlx-cli
+            postgresql_16_jit
+            bunyan-rs
+            nodejs_22
+
+            gnupg
+            git-cliff
+          ] ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      }
     );
 }
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use smd_lib::add;
+
+fn main() {
+    println!("{}", add(3, 4));
+}
