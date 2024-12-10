451494f3e59acd873a678530169dfa771c2eea3d
Flokkq <webcla21@htl-kaindorf.at>
2024-12-10 18:17:58 +0100

    tests(error): verifty that errors are constructed correctly
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- smd-core/src/error.rs
+++ smd-core/src/error.rs
@@ -15,3 +15,33 @@ pub enum Error {
 
 /// Result type of the core library.
 pub type Result<T> = core::result::Result<T, Error>;
+
+#[cfg(test)]
+mod test {
+	use std::{
+		fs,
+		io::ErrorKind,
+		path::PathBuf,
+	};
+
+	use super::*;
+
+	fn mock_function() -> super::Result<String> {
+		let path = PathBuf::from("path_that_does_not_exist");
+		Ok(fs::read_to_string(path)?)
+	}
+
+	#[test]
+	fn throw_parse_error() {
+		let actual_error = mock_function().expect_err("Expected error");
+
+		match actual_error {
+			Error::IoError(e) => {
+				assert_eq!(e.kind(), ErrorKind::NotFound);
+			}
+			_ => {
+				panic!("Unexpected error type");
+			}
+		}
+	}
+}
